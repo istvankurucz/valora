c079c2589a1761b731dcd7c9b6573e83261b6b3e
István Kurucz <kurucz.isti63@gmail.com>
2025-09-29 15:39:50 +0200

    Fonts bundled into project.

--- src/features/navigation/components/layout/SplashScreenController.tsx
+++ src/features/navigation/components/layout/SplashScreenController.tsx
@@ -1,17 +1,15 @@
 import { useAdminUser } from "@/src/features/user/contexts/AdminUserContext";
 import useInitDB from "@/src/hooks/useInitDB";
-import useLoadFonts from "@/src/hooks/useLoadFonts";
 import { SplashScreen } from "expo-router";
 
 const SplashScreenController = () => {
 	// #region Hooks
 	const { loading: loadingMigrations } = useInitDB();
-	const { loading: loadingFonts } = useLoadFonts();
 	const { loading: loadingAdmin } = useAdminUser();
 	//#endregion
 
 	// #region Constants
-	const loading = loadingMigrations || loadingFonts || loadingAdmin;
+	const loading = loadingMigrations || loadingAdmin;
 	//#endregion
 
 	// Hide splash screen
